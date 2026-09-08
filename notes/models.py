363d2ad53c2fcdd39e59b1ea805d56c0c05dad7b
Giovana <giovananzg@al.insper.edu.br>
2026-09-08 10:12:57 -0300

    handout django

---
+++ notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
