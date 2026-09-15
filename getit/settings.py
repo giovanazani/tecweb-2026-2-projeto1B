4231fd4afae2e2740d606a42e8e8a1047048c50a
Giovana <giovananzg@al.insper.edu.br>
2026-09-15 09:09:04 -0300

    debuf

--- getit/settings.py
+++ getit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-66om6$cf9ft+_-8kwl0=vi5lnikkrabbm^i2&2^v2bwh(l@p1e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
